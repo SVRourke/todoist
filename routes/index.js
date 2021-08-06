18c0009024dc4470694b8316e5984b496b75a12a
SVRourke <samuelvanrourke@gmail.com>
2021-08-06 10:00:37 -0400

    import other routers, set up resource structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,17 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+var userRouter = require("./users");
+var listRouter = require("./lists");
+var itemRouter = require("./items");
+listRouter.use("/:listId/items", itemRouter);
+
+router.use("/users", userRouter);
+router.use("/lists", listRouter);
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
