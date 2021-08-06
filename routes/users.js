aeb5cb1541e9fc39076e86339d3686fceadfef16
SVRourke <samuelvanrourke@gmail.com>
2021-08-06 10:02:43 -0400

    refactor

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,10 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+var test = (req, res, next) => {
+  res.send("users");
+};
 
+/* GET users listing. */
+router.get("/", test);
 module.exports = router;
