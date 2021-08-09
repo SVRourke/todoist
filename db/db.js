51adf487758300757a30851c6a88dcf53ffe6618
Samuel Rourke <samuelvanrourke@gmail.com>
2021-08-08 23:15:20 -0400

    if in dev connect to local else connect to atlas

--- db/db.js
+++ db/db.js
@@ -1,4 +1,7 @@
-const url = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.nqbbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const ATLAS = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@cluster0.nqbbk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const LOCAL = `mongodb://${process.env.ATLAS_USER}:${process.env.ATLAS_PASS}@127.0.0.1:27017/todos`;
+
+const url = process.env.NODE_ENV === "dev" ? LOCAL : ATLAS;
 
 const params = {
   useNewUrlParser: true,
