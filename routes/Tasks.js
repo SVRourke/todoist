a87095c83e9b0293d98431cfef5b40b637c9f913
SVRourke <samuelvanrourke@gmail.com>
2021-08-11 08:53:59 -0400

    refactor route actions, import list and task models, actions now using listId

--- routes/Tasks.js
+++ routes/Tasks.js
@@ -1,36 +1,46 @@
 var express = require("express");
-var { Task } = require("../db/models/Tasks");
 var router = express.Router({ mergeParams: true });
 var mongoose = require("mongoose");
+
+var { List } = require("../db/models/Lists");
+var List = mongoose.model("List");
+
+var { Task } = require("../db/models/Tasks");
 var Task = mongoose.model("Task");
 
-var getTodos = (req, res, next) => {
-  Task.find((err, tasks) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(tasks);
-    }
-  });
-};
-
-const newTodo = async (req, res, next) => {
-  // const listId = req.params.listId
-  console.log("params", req.body);
-  const nexTD = await Task.create({ content: req.body.msg });
-  // res.sendStatus(200);
-  res.redirect("back");
-};
 // view all items
-router.get("/", getTodos);
+router.get("/", (req, res, next) => {
+  List.findById(req.params.listId, (err, list) => {
+    if (err) res.json({ error: err });
+    res.json(list.tasks);
+  });
+});
+
 // new item
-router.post("/", newTodo);
+router.post("/new", async (req, res, next) => {
+  List.findById(req.params.listId, (err, list) => {
+    if (err) res.json({ error: err });
+    list.tasks.push(new Task({ content: req.body.task }));
+    list.save();
+    res.json({ list: list });
+  });
+});
 
 // mark item done
 // router.post("/", completeTodo);
 
 // delete item
-// router.delete("/", deleteTodo);
+// router.delete("/:itemId", (req, res) => {
+//   Task.find(
+//     { _id: req.params.itemId },
+//     (err,
+//     (task) => {
+//       if (err) res.json({ error: err });
+//       task.remove();
+//       res.send(301);
+//     })
+//   );
+// });
 
 // update item
 // router.patch("/", updateTodo);
