d34ff6e71ed413a6fcf910a6be4c8cdfde511b14
SVRourke <samuelvanrourke@gmail.com>
2021-08-11 08:50:31 -0400

    fix model import and subdoc declaration

--- db/models/Lists.js
+++ db/models/Lists.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-const Task = mongoose.model("Task");
+const Task = require("./Tasks");
 
 const Lists = new Schema({
   name: String,
-  tasks: [Task],
+  tasks: [Task.schema],
 });
 
 module.exports = mongoose.model("List", Lists);
