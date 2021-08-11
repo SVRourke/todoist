f88aad6482ffc1f67313faeaaffae5664da4ecf7
SVRourke <samuelvanrourke@gmail.com>
2021-08-11 08:53:04 -0400

    import mongoose and list model, stub naiive index and create actions

--- routes/lists.js
+++ routes/lists.js
@@ -1,13 +1,24 @@
 var express = require("express");
 var router = express.Router();
+var mongoose = require("mongoose");
 var itemRouter = require("./Tasks");
+var List = require("../db/models/Lists");
 
 /* Index. */
-router.get("/", function (req, res, next) {
-  res.send("lists");
+router.get("/", (req, res, next) => {
+  List.find((err, lists) => {
+    if (err) res.send(err);
+    res.json(lists);
+  });
 });
 
 /* Create. */
+router.post("/", (req, res) => {
+  List.create({ name: req.body.name }, (err, list) => {
+    if (err) res.json({ error: err });
+    res.json({ list: list });
+  });
+});
 /* Read. */
 /* Update. */
 /* Delete. */
