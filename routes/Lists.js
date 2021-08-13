b9cc9d2d6e9decb69932a63bc05c8daa538501f4
SVRourke <samuelvanrourke@gmail.com>
2021-08-13 12:43:38 -0400

    work on switching actions to lists nested in users

--- routes/Lists.js
+++ routes/Lists.js
@@ -1,22 +1,36 @@
 var express = require("express");
-var router = express.Router();
+var router = express.Router({ mergeParams: true });
 var mongoose = require("mongoose");
 var itemRouter = require("./Tasks");
 var List = require("../models/Lists");
 
 /* Index. */
-router.get("/", (req, res, next) => {
-  List.find((err, lists) => {
-    if (err) res.send(err);
-    res.json(lists);
-  });
-});
+// router.get("/", (req, res, next) => {
+//   User.findById(req.params.userId, (err, user) => {
+//    if (err) throw err;
+
+//   })
+
+//   List.find((err, lists) => {
+//     if (err) res.send(err);
+//     res.json(lists);
+//   });
+// });
 
 /* Create. */
 router.post("/", (req, res) => {
-  List.create({ name: req.body.name }, (err, list) => {
-    if (err) res.json({ error: err });
-    res.json({ list: list });
+  // console.log("PARAMS", req.params);
+  User.findById(req.params.userId, (err, user) => {
+    if (err) throw err;
+
+    // List.create({ name: req.body.name }, (err, list) => {
+    //   if (err) res.json({ error: err });
+    //   user.lists.push(list._id);
+    //   user.save((err, newUser) => {
+    //     if (err) throw err;
+    //     res.json({ list: list });
+    //   });
+    // });
   });
 });
 /* Read. */
